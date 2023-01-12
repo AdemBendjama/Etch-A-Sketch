84b429db9577bd7f8d23d34e6acfb5e8783f4780
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-01-12 11:27:55 +0100

    Add Button to Control Grid Size

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-const GRID_SIZE = 16;
+let GRID_SIZE = 16;
 const CONTAINER = document.querySelector('.container');
+const GRID_SIZE_CONTROLLER = document.querySelector(".grid-size");
 
 
-function createDivs(){
+function createDivs() {
     let divs = new Array();
-    for (let i = 0; i < GRID_SIZE*GRID_SIZE; i++) {
+    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
         const div = document.createElement("div");
         CONTAINER.appendChild(div);
-        divs[i]=div;
+        divs[i] = div;
     }
 
     impowerDivs(divs);
@@ -15,17 +16,57 @@ function createDivs(){
 
 }
 
-function impowerDivs(divs){
+function impowerDivs(divs) {
     //
     for (const div of divs) {
-        div.addEventListener("mouseover",(e) =>{
+        div.addEventListener("mouseover", (e) => {
             console.log(e);
             e.target.style.cssText = "background-color:black;"
         });
     }
 }
 
-window.onload=()=> {
+function impowerButtons() {
+    //
+    GRID_SIZE_CONTROLLER.addEventListener("change", (e) => changeGridSize(e.target.value));
+}
+
+function removeDivs() {
+    //
+    CONTAINER.innerHTML = "";
+}
+
+function reset(){
+    //
+    GRID_SIZE = 16;
+    GRID_SIZE_CONTROLLER.value=16;
+    removeDivs();
+    createDivs();
+    changeCssGridLayout(GRID_SIZE);
+    
+    
+}
+
+function changeGridSize(size) {
+    //
+    if (size >= 1 && size <= 100) {
+        GRID_SIZE = size;
+        changeCssGridLayout(GRID_SIZE);
+        removeDivs();
+        createDivs();
+    }else{
+        reset();
+    }
+}
+
+function changeCssGridLayout(size) {
+    //
+    CONTAINER.style.cssText = `grid-template-columns: repeat(${size},1fr);
+                               grid-template-rows: repeat(${size},auto);`;
+}
+
+window.onload = () => {
     //
     createDivs();
+    impowerButtons();
 }
